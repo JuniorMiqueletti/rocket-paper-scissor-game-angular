b1359eba20036b948736937631d55ecc83ae9373
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-09-11 08:42:17 -0300

    removing some tests and adjusting bot.service test

--- src/app/bot.service.spec.ts
+++ src/app/bot.service.spec.ts
@@ -12,4 +12,27 @@ describe('BotService', () => {
   it('should be created', inject([BotService], (service: BotService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should be returned', inject([BotService], (service: BotService) => {
+    service.choice()
+    .then((result) => expect(result));
+  }));
+
+  it('should be returned a number between 0 and 2', inject([BotService], (service: BotService) => {
+      service.choice()
+      .then((result) => {
+        expect(result).toBeGreaterThanOrEqual(0);
+        expect(result).toBeLessThanOrEqual(2);
+      });
+  }));
+
+  it('should be returned a number between 0 and 2 call service 100 times', inject([BotService], (service: BotService) => {
+    for (let i = 0; i < 100; i++) {
+      service.choice()
+      .then((result) => {
+        expect(result).toBeGreaterThanOrEqual(0);
+        expect(result).toBeLessThanOrEqual(2);
+      });
+    }
+  }));
 });
